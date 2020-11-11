87d7794b96b4fa44da4e3d6cfb425f3c41a05368
Samuel de Morais <samucatnbpratic@gmail.com>
2020-11-11 10:01:45 -0300

    iniciado projeto 2 de estudo

---
+++ ConsoleParte2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleParte2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            TimeSpan t1 = new TimeSpan(1, 30, 10);
+
+            //aqui fizemos a multiplicação por 2.0 como utilizamos um double, fizemos o cast para poder dar certo.
+            TimeSpan mult = TimeSpan.FromTicks( ((long)((double)t1.Ticks * 2.0)));
+            Console.WriteLine(t1);
+            Console.WriteLine(mult);
+            Console.ReadLine();
+        }
+    }
+}
