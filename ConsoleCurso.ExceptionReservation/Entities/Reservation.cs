976fd7ffbf7cba2ae8f78ee6bfd21a2cb0c557e3
Samuel de Morais <samucatnbpratic@gmail.com>
2020-11-16 17:20:15 -0300

    projeto Reservation tratando exceptions

---
+++ ConsoleCurso.ExceptionReservation/Entities/Reservation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using ConsoleCurso.ExceptionReservation.Entities.Exceptions;
+
+namespace ConsoleCurso.ExceptionReservation.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+
+        public DateTime CheckIn { get; set; }
+        
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            //vamos pegamos a diferença entre um instante e outro.
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays; // pegamos essa diferença em dias
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            DateTime now = DateTime.Now;
+            if (checkin <= now || checkout <= now)
+            {
+                throw new DomainException ( "Informe datas futuras para atualizar reserva!");
+            }
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + "  nights";
+        }
+    }
+}
---
+++ ConsoleCurso.ExceptionReservation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ConsoleCurso.ExceptionReservation.Entities;
+using ConsoleCurso.ExceptionReservation.Entities.Exceptions;
+
+namespace ConsoleCurso.ExceptionReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.Write("Room Number: ");
+                int numero = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime e = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime s = DateTime.Parse(Console.ReadLine());
+
+                Reservation reserva = new Reservation(numero, e, s);
+                Console.WriteLine(reserva);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                e = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                s = DateTime.Parse(Console.ReadLine());
+
+                reserva.UpdateDates(e, s);
+                Console.WriteLine(reserva);
+            }
+            catch (DomainException e)
+            {
+
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+        }
+
+
+    }
+}
+
