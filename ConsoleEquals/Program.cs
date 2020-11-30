77f7dab482c17a0f83af85e04be0ad7e02916bbd
Samuel de Morais <samucatnbpratic@gmail.com>
2020-11-30 16:37:07 -0300

    novo projeto utilizando Equals e GetHashCode

---
+++ ConsoleEquals/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ConsoleEquals.Entities;
+
+namespace ConsoleEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Nome = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Nome = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+            Console.WriteLine(a == b);//compara referencia de memoria 
+        }
+    }
+}
