f2de9ca850b9a24108aa6c5296bab35b149543e4
Samuel de Morais <samucatnbpratic@gmail.com>
2020-11-30 14:23:08 -0300

    criando projeto Generics parte 1

---
+++ ConsoleGenericParte1/PrintService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGenericParte1
+{
+    class PrintService
+    {
+        private int[] values = new int[10];
+        private int count = 0;
+
+        public void AddValue(int value)
+        {
+            if (count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            values[count] = value;
+            count++;
+        }
+
+        public int First()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Value is null");
+            }
+            return values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < count -1; i++)
+            {
+                Console.Write(values[i] + ", ");
+            }
+            if (count > 0)
+            {
+                Console.Write(values[count - 1]);
+
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ ConsoleGenericParte1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleGenericParte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService p = new PrintService();
+
+            Console.Write("How many values ? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                p.AddValue(x);
+            }
+
+            p.Print();
+            Console.WriteLine("First: " + p.First());
+        }
+    }
+}
