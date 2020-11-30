f893dfd48da3a7486b104ce40d525dab5b26e2f0
Samuel de Morais <samucatnbpratic@gmail.com>
2020-11-30 16:50:14 -0300

    novo projeto ConsoleHashSet utilizando coleção HashSet

---
+++ ConsoleHashSet/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Computer"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
