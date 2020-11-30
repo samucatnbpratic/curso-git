1db5289f5b38a5d002dd80cd05ec4596f02d1725
Samuel de Morais <samucatnbpratic@gmail.com>
2020-11-30 11:48:32 -0300

    novo projeto ConsoleIComparable utilizando interface IComparable

---
+++ ConsoleIComparable/Entities/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ConsoleIComparable.Entities
+{
+    class Funcionario: IComparable
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string csvFuncionario)
+        {
+            string[] vect = csvFuncionario.Split(',');
+            Nome = vect[0];
+            Salario = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Funcionario))
+            {
+                throw new ArgumentException("Erro comparando: objeto não é do tipo funcionario");
+            }
+            Funcionario f = obj as Funcionario;
+            return Salario.CompareTo(f.Salario);
+
+
+        }
+    }
+}
---
+++ ConsoleIComparable/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ConsoleIComparable.Entities;
+
+namespace ConsoleIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ListaNomes();
+            ListaNomeSalario();
+        }
+
+        static void ListaNomeSalario()
+        {
+            string path = @"d:\temp\listanomesalario.csv";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionario> list = new List<Funcionario>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Funcionario(sr.ReadLine()));
+                    }
+                    list.Sort(); //ordenamos a lista
+                    foreach (Funcionario item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("Ocorreu um erro! ");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        static void ListaNomes()
+        {
+            string path = @"d:\temp\listanomes.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort(); //ordenamos a lista
+                    foreach (string item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("Ocorreu um erro! ");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
