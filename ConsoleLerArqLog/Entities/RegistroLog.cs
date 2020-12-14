4ef3ff14ba3ea81685cd76fc72ec8c3559fcbe95
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-14 16:27:57 -0300

    Novo projeto ConsoleLerArqLog explorando HashSet

---
+++ ConsoleLerArqLog/Entities/RegistroLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleLerArqLog.Entities
+{
+    class RegistroLog
+    {
+        public string Usuario { get; set; }
+        public DateTime Instante { get; set; }
+
+        public RegistroLog(string usuario, DateTime instante)
+        {
+            Usuario = usuario;
+            Instante = instante;
+        }
+
+        public override int GetHashCode()
+        {
+            return Usuario.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is RegistroLog))
+            {
+                return false;
+            }
+
+            RegistroLog other = obj as RegistroLog; //downcasting
+            return Usuario.Equals(other.Usuario);
+        }
+    }
+}
--- ConsoleLerArqLog/Program.cs
+++ ConsoleLerArqLog/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using ConsoleLerArqLog.Entities;
 
 namespace ConsoleLerArqLog
 {
@@ -7,25 +9,29 @@ namespace ConsoleLerArqLog
     {
         static void Main(string[] args)
         {
+            //HashSet - estamos usando ele e nao o Sortedset porque a ordem não importa e ele neste caso é o mais rapido
+            HashSet<RegistroLog> set = new HashSet<RegistroLog>();
+
             string path = @"d:\temp\log.txt";
-            StreamReader sr = null;
             try
             {
-                sr = File.OpenText(path);
-                while (!sr.EndOfStream)
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        //observação nao entra registros com nome de usuario repetido
+                        set.Add(new RegistroLog(line[0], DateTime.Parse(line[1])));
+                        
+                    }
+
+                    Console.WriteLine("Total de usuarios: " + set.Count);
                 }
             }
             catch (IOException e)
             {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if (sr != null) sr.Close();
             }
         }
     }
