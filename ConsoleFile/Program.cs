761c2424b8a2ece604ca15f2390807f9c07e9feb
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-16 12:04:25 -0300

    adicionado novos metodos para utilizar FileStream, StreamReader, StreamWriter

--- ConsoleFile/Program.cs
+++ ConsoleFile/Program.cs
@@ -12,6 +12,98 @@ namespace ConsoleFile
             //File - classe estatica porém existe uma verificação de segurança o que torna mais demorada que FileInfo
             //FileInfo - deve ser instanciada e não tem a verificação de segurança.
 
+            // PrimeiroExemplo();
+
+            SegundoExemplo();
+
+            TerceiroExemplo();
+
+            QuartoExemplo(); //vamos usar o StreamWrite
+
+        }
+
+        private static void QuartoExemplo()
+        {
+            string sourcePath = @"d:\temp\file1.txt";
+            string targetPath = @"d:\temp\file2.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+        private static void TerceiroExemplo()
+        {
+            string sourcePath = @"d:\temp\file1.txt";
+            try
+            {
+
+                using (FileStream fs = new FileStream(sourcePath, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+
+                    }
+
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+        private static void SegundoExemplo()
+        {
+            string sourcePath = @"d:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                fs = new FileStream(sourcePath, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+
+        private static void PrimeiroExemplo()
+        {
             string sourcePath = @"d:\temp\file1.txt";
             string targetPath = @"d:\temp\file2.txt";
             try
@@ -32,8 +124,6 @@ namespace ConsoleFile
                 Console.WriteLine(e.Message);
 
             }
-
-
 
         }
     }
