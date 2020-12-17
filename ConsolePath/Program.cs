b1da3fe34405c677365b293b4d9a8efeccd015d6
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-17 15:50:13 -0300

    Novo projeto ConsolePath demonstração de alguns metodos desta classe

---
+++ ConsolePath/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsolePath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"d:\temp\file1.txt";
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+
+        }
+    }
+}
