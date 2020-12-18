b8543d8037652933c2d9a5b7e0754d1ab655dbf5
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-18 17:17:52 -0300

    ConsoleDelegateAction passando agora expressao lambda sem criar um metodo para isso

--- ConsoleDelegateAction/Program.cs
+++ ConsoleDelegateAction/Program.cs
@@ -14,7 +14,9 @@ namespace ConsoleDelegateAction
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> act = UdpatePrice;
+            //vamos agora utilizar expressao lambda
+            //obs: como é void  utilizamos as chaves para determinar o inicio e fim do corpo
+            Action<Product> act = p => { p.Price += p.Price * 0.1; };
 
             list.ForEach(act);
 
