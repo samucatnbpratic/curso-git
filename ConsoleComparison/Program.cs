85a462f9f0d38f3a146de523b5ea6c166811b431
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-18 15:52:37 -0300

    passamos uma expressão lambda inline

--- ConsoleComparison/Program.cs
+++ ConsoleComparison/Program.cs
@@ -14,11 +14,8 @@ namespace ConsoleComparison
             list.Add(new Product("Notebook", 1200.00m));
             list.Add(new Product("Tablet", 450.00m));
 
-            //declaramos uma variavel e ela recebe uma expressão lambda (declaração de função anonima)
-            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-            //Comparison - é um delegate que é uma referencia para função type safety
-            //observe que não precisamos informar nossos dois parametros da função.
-            list.Sort(comp);
+            //passamos uma expressão lambda inline
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach (Product p in list)
             {
