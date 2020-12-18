2b2ea39708c251d60db238e2d834a8c4550ef910
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-18 16:22:40 -0300

    Projeto ConsoleDelegates aprendendo criar e referenciar

---
+++ ConsoleDelegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ConsoleDelegates.Services;
+
+namespace ConsoleDelegates
+{
+    class Program
+    {
+        //declaração de um delegate 
+        //ele será referencia para uma função que recebe dois numeros double e retorna um double
+        delegate double BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op; 
+            
+
+            double result = 0;
+            result = CalculationService.Sum(a, b);
+            Console.WriteLine("SUM: " + result);
+            //usando o delegate, ele é uma referencia para função
+            op = CalculationService.Sum;
+            Console.WriteLine("DELEGATE SUM: " + op(a, b));
+            //sintaxe alternativa
+            Console.WriteLine("DELEGATE (INVOKE) SUM: " + op.Invoke(a, b));
+
+            result = CalculationService.Max(a, b);
+            Console.WriteLine("MAX: " + result);
+            //usando o delegate, ele é uma referencia para função
+            op = CalculationService.Max;
+            Console.WriteLine("DELEGATE MAX: " + op(a, b));
+
+            result = CalculationService.Square(a);
+            Console.WriteLine("SQUARE: " + result);
+            //OBS: aqui não podemos usar o delegate porque numero de parametros da função Square é diferente.
+        }
+    }
+}
