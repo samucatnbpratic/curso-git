906bf417b2698b520f1b8c5c7a6bffe10f7f92ae
Samuel de Morais <samucatnbpratic@gmail.com>
2020-12-21 08:46:13 -0300

    Projeto ConsoleDelegateFunc utilizando o delegate Func

---
+++ ConsoleDelegateFunc/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ConsoleDelegateFunc.Entities;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objetivo deste projeto é utilizarmos o Delegate Func
+            //pra isso estamos usando a função Select do List
+
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameUpper).ToList();
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
